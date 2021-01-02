7959004a459d1df952d25ee01a3d68b3370eddef
darkboy03 <darkboy03@naver.com>
2021-01-02 19:50:21 +0900

    2021년1월2일

--- src/test/java/kod/springboot/web/HelloControllerTest.java
+++ src/test/java/kod/springboot/web/HelloControllerTest.java
@@ -13,6 +13,7 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -29,6 +30,20 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
 
+    }
+
+    @Test
+    public void helloDto가_리턴되다() throws Exception {
+        String name ="hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 }
