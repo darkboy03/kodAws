6f561d0c4984900b23d2cb5a20a1f67d4d22f0ee
darkboy03 <darkboy03@naver.com>
2021-01-09 10:48:36 +0900

    2021년1월9일

--- src/main/java/kod/springboot/service/posts/PostsService.java
+++ src/main/java/kod/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package kod.springboot.service.posts;
 
 import kod.springboot.domain.posts.Posts;
 import kod.springboot.domain.posts.PostsRepository;
+import kod.springboot.web.dto.PostsListResponseDto;
 import kod.springboot.web.dto.PostsResponseDto;
 import kod.springboot.web.dto.PostsSaveRequestDto;
 import kod.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -36,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+
+        postsRepository.delete(posts);
     }
 }
